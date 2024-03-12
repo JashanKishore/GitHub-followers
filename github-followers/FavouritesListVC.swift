37861c1b60b3f12db18a33191e3f78be178427e0
Jashan Kishore <jashan.kishore@gmail.com>
2024-03-12 15:58:14 +1100

    Added view controllers for search and favourites with different background colours.

---
+++ github-followers/FavouritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavouritesListVC.swift
+//  github-followers
+//
+//  Created by Jashan kishore on 12/3/2024.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

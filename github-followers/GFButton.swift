358f47fcff2d3f5ca8a0e122dfc05bfcfcedeb6b
Jashan Kishore <jashan.kishore@gmail.com>
2024-03-12 16:34:17 +1100

    Creating custom button, added custom initialiser to allow changing of button title and bg colour.

---
+++ github-followers/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  github-followers
+//
+//  Created by Jashan kishore on 12/3/2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColour: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColour
+        self.setTitle(title, for: .normal)
+    }
+    
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+}

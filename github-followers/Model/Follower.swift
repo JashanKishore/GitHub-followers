6414cbb2e0b8b1163fa48ae2428ffac87db39b07
Jashan Kishore <jashan.kishore@gmail.com>
2024-03-15 11:15:26 +1100

    Created model files for followers and users.

---
+++ github-followers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  github-followers
+//
+//  Created by Jashan kishore on 15/3/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
